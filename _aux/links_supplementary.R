7230cdea48dfe466776e4d8fbb1e02629325694c
Erfan <erfanmeskar@gmail.com>
2025-05-29 11:33:19 -0400

    update header format and TA photo

--- _aux/links_supplementary.R
+++ _aux/links_supplementary.R
@@ -1,4 +1,10 @@
+course_code <- "CSC(MAT)A67"
+course_full_name <- "Discrete Mathematics"
+university <- "University of Toronto Scarborough"
+department <- "Computer & Mathematical Science"
+season <- "Summer"
+year <- "2024"
+
 zoom_lec_l <- "https://utoronto.zoom.us/j/83250464222"
 quercus_l <- "https://q.utoronto.ca/courses/348221"
-markus_l <- "https://markus0.teach.cs.toronto.edu/utm-2025-01/?locale=en"
 crowdmark_l <- "https://crowdmark.com"
